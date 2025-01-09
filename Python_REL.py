ef9a7327bc79625545b19ba9dd920ef6347c0f8a
Rich Tsai <richard.tsai@onepeloton.com>
2025-01-09 11:28:44 +0800

    quick example
    completed the quick example

--- Python_REL.py
+++ Python_REL.py
@@ -12,4 +12,15 @@ dist = Weibull_Distribution(alpha=30, beta=2)  # creates the distribution object
 data = dist.random_samples(20, seed=42)  # draws 20 samples from the distribution. Seeded for repeatability
 plt.subplot(121) # which means row 1 column 2 index 1 plot empty frame 
 fit = Fit_Weibull_2P(failures=data)  # fits a Weibull distribution to the data and generates the probability plot
-#plt.show() # show the plot 
+plt.subplot(122)
+fit.distribution.SF(label='fitted distribution')  # uses the distribution object from Fit_Weibull_2P and plots the survival function
+dist.SF(label='original distribution', linestyle='--') # plots the survival function of the original distribution
+plot_points(failures=data, func='SF')  # overlays the original data on the survival function
+plt.legend() # to display the legned 
+plt.show() # show the plot 
+
+### this portion of code is to figure out the purpose of "seed"
+#data1 = dist.random_samples(20) 
+#plt.subplot(122)
+#fit = Fit_Weibull_2P(failures=data1)
+#plt.show()
